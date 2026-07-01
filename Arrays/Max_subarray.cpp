9b7a52e172cfa49e78cb433e039eed7c0fed0521
shaurya-b1 <bishnoishaurya0@gmail.com>
2026-07-01 13:29:40 +0530

    max sub_array

---
+++ Arrays/Max_subarray.cpp
@@ -0,0 +1,12 @@
+
+Problem Description
+The "Maximum SubArray" is a well-known problem in algorithm design, often encountered in technical interviews. The challenge is to determine the contiguous subarray within a one-dimensional array of numbers nums that results in the largest sum. The goal is to find this sum efficiently, particularly when the array includes both positive and negative integers.
+
+Given an integer array nums, your task is to compute the sum of the subarray which has the largest sum among all subarrays.
+
+Example
+Suppose you have the array nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]. When you sum the subarray [4, -1, 2, 1], you get 6, which is the largest possible sum among contiguous subarrays. Hence, the expected result for this input is 6.
+
+Additional Notes
+It's important for your solution to efficiently work, even when dealing with a large array size up to 30,000 elements. Proper algorithmic design is crucial for achieving a time complexity of O(n).
+
